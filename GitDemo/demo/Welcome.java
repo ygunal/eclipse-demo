28479590d9345c1966d0251eb6274ee3fe871e7a
Yuksel Gunal <yuksel.gunal@marist.edu>
2020-05-04 18:02:31 -0400

    Eclipse demo

---
+++ GitDemo/demo/Welcome.java
@@ -0,0 +1,14 @@
+package demo;
+
+/**
+ * This is the very first program we looked at this semester.
+ * 
+ * @author yuksel
+ *
+ */
+public class Welcome {
+	public static void main(String[] args) {
+		// Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+	}
+}
